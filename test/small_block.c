b8309236f6532af36e2d71a0e1394c9bf503e8f0
skienzle <steffen.kienzle5@googlemail.com>
2024-05-02 23:05:14 +0200

    Add tests for small_block

--- test/small_block.c
+++ test/small_block.c
@@ -10,6 +10,8 @@
 
 #define NUM_BLOCKS 50
 
+
+
 static const char*
 test_small_block_list_(t_small_block** blocks) {
     (void)blocks;
@@ -80,7 +82,45 @@ test_split_small_block_(t_small_block** blocks) {
 }
 
 static const char*
-test_merge_small_block();
+test_merge_small_block(t_small_block** blocks) {
+    t_small_block* block = get_next_small_block(*blocks);
+    t_small_block* next = get_next_small_block(block);
+    if (block == NULL || next == NULL || is_allocated(next->curr)) {
+        return "Not enough blocks to test";
+    }
+    t_small_block* old_ptr = block;
+    t_small_block old_block = *block;
+    merge_small_block(&block, false);
+    next = get_next_small_block(block);
+    if (block != old_ptr) {
+        return "Pointer to block was moved when it shouln't have been";
+    } else if (next != NULL && !is_allocated(next->curr)) {
+        return "Block not merged with next free block";
+    } else if (get_block_size(block->curr) <= get_block_size(old_block.curr)) {
+        return "Block size not properly updated";
+    } else if (is_allocated(old_block.curr) != is_allocated(block->curr)) {
+        return "Is allocated flag not transferred correctly";
+    }
+
+    old_ptr = block;
+    old_block = *block;
+    t_small_block* prev = get_prev_small_block(block);
+    if (is_allocated(prev->curr)) {
+        return "Block layout unsuitable";
+    }
+    merge_small_block(&block, true);
+    prev = get_prev_small_block(block);
+    if (block == old_ptr) {
+        return "Pointer to block not updated";
+    } else if (prev != NULL && !is_allocated(prev->curr)) {
+        return "Block not merged mit previous free block";
+    } else if (get_block_size(block->curr) <= get_block_size(old_block.curr)) {
+        return "Block size not properly updated";
+    } else if (is_allocated(old_block.curr) != is_allocated(block->curr)) {
+        return "Is allocated flag not transferred correctly";
+    }
+    return NULL;
+}
 
 static const char*
 test_allocate_small_block_(t_small_block** blocks) {
@@ -109,6 +149,51 @@ test_allocate_small_block_(t_small_block** blocks) {
 }
 
 static const char*
+test_reallocate_small_block_(t_small_block** blocks) {
+    t_small_block* block = *blocks;
+    for (size_t i = 0; i < NUM_BLOCKS; ++i) {
+        if (block == NULL) {
+            return "Not enough blocks to test with";
+        }
+        const size_t new_size = ALIGN_ALLOC_SIZE(
+            (i * 2) % TINY_ALLOC_BLOCK_SIZE,
+            FT_MALLOC_ALIGNMENT
+        );
+        char* data = get_small_block_data(block);
+        size_t size = get_block_size(block->curr);
+        const size_t pos = i % size;
+        const char c = (i % 26) + 'A';
+        data[0] = c;
+        data[pos] = c;
+        data[size - 1] = c;
+        if (reallocate_small_block(block, new_size, *blocks) == NULL) {
+            return "Page not suitable for testing";
+        }
+        if (get_block_size(block->curr) < new_size) {
+            return "Size not big enough after realloc";
+        } else if (data[0] != c || data[pos] != c || data[size - 1] != c) {
+            return "Data not properly copied";
+        }
+        block = get_next_small_block(block);
+    }
+    return NULL;
+}
+
+static const char*
+test_deallocate_small_block_(t_small_block** blocks) {
+    t_small_block* block = *blocks;
+    for (size_t i = 0; i < NUM_BLOCKS; ++i) {
+        if (block == NULL) {
+            return "Not enough blocks to test with";
+        }
+        
+
+        block = get_next_small_block(block);
+    }
+    return NULL;
+}
+
+static const char*
 test_get_small_block_data_(t_small_block** blocks) {
     for (t_small_block* block = *blocks; block != NULL;) {
         void* data = get_small_block_data(block);
@@ -121,8 +206,6 @@ test_get_small_block_data_(t_small_block** blocks) {
     }
     return NULL;
 }
-
-
 
 
 static void
