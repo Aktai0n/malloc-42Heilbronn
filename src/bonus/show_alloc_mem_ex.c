5d5d119acb2ffab85f3072e602b9f481a1fc8951
skienzle <steffen.kienzle5@googlemail.com>
2025-01-08 21:46:33 +0100

    Implement show_alloc_mem_ex

--- src/bonus/show_alloc_mem_ex.c
+++ src/bonus/show_alloc_mem_ex.c
@@ -1,8 +1,3 @@
-
-
-
-#if 1
-
 #include "ft_malloc_internal.h"
 #include "libft.h"
 #include "colors.h"
@@ -14,23 +9,52 @@
 
 #include <stdint.h>
 
+static void print_bytes_(int fd, void* ptr, size_t size, const char* color) {
+    if (!isatty(fd)) {
+        color = NULL;
+    }
+    const size_t row_len = FT_MALLOC_ALIGNMENT;
+    size_t i = 0;
+    for (;(i + row_len) < size; i += row_len) {
+        void* pos = (void*)((uintptr_t)ptr + i);
+        ft_dprintf(fd, "%p ", pos);
+        if (color != NULL) {
+            ft_putstr_fd(color, fd);
+        }
+        ft_malloc_print_bytes(fd, pos, row_len);
+        if (color != NULL) {
+            ft_putstr_fd(RESET_COLOR, fd);
+        }
+        ft_putstr_fd("\n", fd);
+    }
+    void* pos = (void*)((uintptr_t)ptr + i);
+    ft_dprintf(fd, "%p ", pos);
+    if (color != NULL) {
+        ft_putstr_fd(color, fd);
+    }
+    ft_malloc_print_bytes(fd, pos, size - i);
+    if (color != NULL) {
+        ft_putstr_fd(RESET_COLOR, fd);
+    }
+    ft_putstr_fd("\n", fd);
+}
+
 static void print_small_block_(int fd, t_small_block* block) {
-    ft_putstr_fd(BOLD_INTENSE_CYAN_COLOR, fd);
-    ft_malloc_print_bytes(fd, block, sizeof(*block));
+    print_bytes_(fd, block, sizeof(*block), BOLD_INTENSE_CYAN_COLOR);
     void* data = get_small_block_data(block);
     size_t size = get_block_size(block->curr);
+    const char* color = NULL;
     if (is_allocated(block->curr)) {
-        ft_putstr_fd(BOLD_INTENSE_RED_COLOR, fd);
+        color = BOLD_INTENSE_RED_COLOR;
     } else {
-        ft_putstr_fd(BOLD_INTENSE_GREEN_COLOR, fd);
+        color = BOLD_INTENSE_GREEN_COLOR;
     }
-    ft_malloc_print_bytes(fd, data, size);
+    print_bytes_(fd, data, size, color);
 }
 
 static void print_small_(int fd, t_small_page* pages) {
     for (t_small_page* page = pages; page != NULL; page = page->next) {
-        ft_putstr_fd(BOLD_INTENSE_BLUE_COLOR, fd);
-        ft_malloc_print_bytes(fd, page, sizeof(*page));
+        print_bytes_(fd, page, sizeof(*page), BOLD_INTENSE_BLUE_COLOR);
         for (t_small_block* block = page->block_list; block != NULL;) {
             print_small_block_(fd, block);
             block = get_next_small_block(block);
@@ -39,16 +63,16 @@ static void print_small_(int fd, t_small_page* pages) {
 }
 
 static void print_medium_block_(int fd, t_medium_block* block) {
-    ft_putstr_fd(BOLD_INTENSE_CYAN_COLOR, fd);
-    ft_malloc_print_bytes(fd, block, sizeof(*block));
+    print_bytes_(fd, block, sizeof(*block), BOLD_INTENSE_CYAN_COLOR);
     void* data = get_medium_block_data(block);
     size_t size = get_block_size(block->curr);
+    const char* color = NULL;
     if (is_allocated(block->curr)) {
-        ft_putstr_fd(BOLD_INTENSE_RED_COLOR, fd);
+        color = BOLD_INTENSE_RED_COLOR;
     } else {
-        ft_putstr_fd(BOLD_INTENSE_GREEN_COLOR, fd);
+        color = BOLD_INTENSE_GREEN_COLOR;
     }
-    ft_malloc_print_bytes(fd, data, size);
+    print_bytes_(fd, data, size, color);
 }
 
 static t_medium_block* get_first_medium_block_(t_medium_page* page) {
@@ -75,8 +99,7 @@ static t_medium_block* get_first_medium_block_(t_medium_page* page) {
 
 static void print_medium_(int fd, t_medium_page* pages) {
     for (t_medium_page* page = pages; page != NULL; page = page->next) {
-        ft_putstr_fd(BOLD_INTENSE_BLUE_COLOR, fd);
-        ft_malloc_print_bytes(fd, page, sizeof(*page));
+        print_bytes_(fd, page, sizeof(*page), BOLD_INTENSE_BLUE_COLOR);
         t_medium_block* block = get_first_medium_block_(page);
         while (block != NULL) {
             print_medium_block_(fd, block);
@@ -86,104 +109,21 @@ static void print_medium_(int fd, t_medium_page* pages) {
 }
 
 static void print_large_(int fd, t_large_page* pages) {
-    for (t_large_page* page; page != NULL; page = page->next) {
-        ft_putstr_fd(BOLD_INTENSE_CYAN_COLOR, fd);
-        ft_malloc_print_bytes(fd, page, sizeof(page));
+    for (t_large_page* page = pages; page != NULL; page = page->next) {
+        print_bytes_(fd, page, sizeof(*page), BOLD_INTENSE_CYAN_COLOR);
         void* data = get_large_page_data(page);
         size_t size = get_large_page_data_size(page);
-        ft_putstr_fd(BOLD_INTENSE_GREEN_COLOR, fd);
-        ft_malloc_print_bytes(fd, data, size);
+        print_bytes_(fd, data, size, BOLD_INTENSE_RED_COLOR);
         t_large_page_end* end = get_large_page_end(page);
-        ft_putstr_fd(BOLD_INTENSE_CYAN_COLOR, fd);
-        ft_malloc_print_bytes(fd, end, sizeof(end));
+        print_bytes_(fd, end, sizeof(*end), BOLD_INTENSE_CYAN_COLOR);
     }
 }
 
 void show_alloc_mem_ex(void) {
     struct s_heap* heap = &g_heap;
-    int fd = STDIN_FILENO;
+    int fd = STDOUT_FILENO;
 
     print_small_(fd, heap->small_pages);
     print_medium_(fd, heap->medium_pages);
     print_large_(fd, heap->large_pages);
 }
-
-#else
-
-#include <stdint.h>
-
-#include "ft_malloc.h"
-#include "defines.h"
-#include "colors.h"
-#include "libft.h"
-#include "../memory/memory_page/memory_page.h"
-#include "../memory/alloc_block/alloc_block.h"
-
-static void print_bytes_(void* ptr, size_t size) {
-    uint8_t* bytes = (uint8_t*)ptr;
-    for (size_t i = 0; i < size; ++i) {
-        ft_printf(" %b ", bytes[i]);
-    }
-}
-
-static void print_alloc_block_(t_alloc_block* block) {
-    const size_t size = get_alloc_size(block);
-    ft_printf("block from %p to %p, size %u, is_allocated: %d, is_last_block: %d\n",
-        block,
-        (void*)((size_t)block + sizeof(*block) + size),
-        size,
-        is_allocated_block(block),
-        is_last_alloc_block(block)
-    );
-    ft_putstr(BOLD_PURPLE_COLOR);
-    print_bytes_(block, sizeof(*block));
-    if (is_allocated_block(block)) {
-        ft_putstr(BOLD_RED_COLOR);
-    } else {
-        ft_putstr(BOLD_GREEN_COLOR);
-    }
-    print_bytes_(get_alloc_data(block), size);
-    ft_printf("%s\n", RESET_COLOR);
-}
-
-static void print_alloc_blocks_(t_alloc_block* blocks) {
-    for (t_alloc_block* block = blocks; block != NULL;) {
-        print_alloc_block_(block);
-        block = get_next_block_in_memory(block);
-    }
-}
-
-static void print_memory_page_(t_memory_page* page) {
-    ft_printf("page from %p to %p\n", page, (char*)page + page->size);
-    ft_putstr(BOLD_YELLOW_COLOR);
-    uint8_t* bytes = (uint8_t*)page;
-    for (size_t i = 0; i < sizeof(*page); ++i) {
-        ft_printf(" %b ", bytes[i]);
-    }
-    ft_printf("%s\n", RESET_COLOR);
-    if (
-        page->allocated_list != NULL &&
-        (size_t)page->allocated_list < (size_t)page->free_list
-    ) {
-        print_alloc_blocks_(page->allocated_list);
-    } else {
-        print_alloc_blocks_(page->free_list);
-    }
-    ft_printf("%s\n", RESET_COLOR);
-}
-
-static void print_memory_pages_(t_memory_page* pages) {
-    for (t_memory_page* page = pages; page != NULL; page = page->next) {
-        print_memory_page_(page);
-    }
-}
-
-void show_alloc_mem_ex(void) {
-    ft_putstr_color(RED_COLOR, "show_alloc_mem_ex\n");
-    // t_memory_page* pages = g_heap.tiny_pages;
-    ft_putstr_color(CYAN_COLOR, "tiny pages:\n");
-    print_memory_pages_(g_heap.tiny_pages);
-    print_memory_pages_(g_heap.small_pages);
-    print_memory_pages_(g_heap.large_pages);
-}
-#endif
